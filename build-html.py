9b08bae1ed3b4464a8f77c366a21bd3711b8a56b
Monte Mishkin <monte@mishkin.com>
2025-11-12 16:57:33 -0800

    next jam!

--- build-html.py
+++ build-html.py
@@ -13,7 +13,7 @@ NEXT_JAM_DETAILS = """
   <ul>
     <li>
       <span>When:&nbsp;</span>
-      Sun, Oct 26 from 12-4pm
+      Sun, Nov 17 from 12-4pm
     </li>
     <li>
       &nbsp;&nbsp;&nbsp;&nbsp;12-2pm: Pre-Production and Lunch!
@@ -23,7 +23,7 @@ NEXT_JAM_DETAILS = """
     </li>
     <li>
       <span>Where:</span>
-      Marina District, SF (exact location tbd)
+      Outer Richmond, SF (exact location tbd)
     </li>
     <li>
       <span>Theme:</span>
